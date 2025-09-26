513648aefe814ca7b6027c0d64629c102971f5a3
pratikvats7 <pratikfake420@gmail.com>
2025-09-26 10:42:01 +0530

    Default Constructor

--- build/generated-sources/wsdl2java/com/example/generated/ws/CustomerType.java
+++ build/generated-sources/wsdl2java/com/example/generated/ws/CustomerType.java
@@ -5,6 +5,17 @@ import jakarta.xml.bind.annotation.XmlAccessType;
 import jakarta.xml.bind.annotation.XmlAccessorType;
 import jakarta.xml.bind.annotation.XmlElement;
 import jakarta.xml.bind.annotation.XmlType;
+import org.jvnet.jaxb.lang.Equals;
+import org.jvnet.jaxb.lang.EqualsStrategy;
+import org.jvnet.jaxb.lang.HashCode;
+import org.jvnet.jaxb.lang.HashCodeStrategy;
+import org.jvnet.jaxb.lang.JAXBEqualsStrategy;
+import org.jvnet.jaxb.lang.JAXBHashCodeStrategy;
+import org.jvnet.jaxb.lang.JAXBToStringStrategy;
+import org.jvnet.jaxb.lang.ToString;
+import org.jvnet.jaxb.lang.ToStringStrategy;
+import org.jvnet.jaxb.locator.ObjectLocator;
+import org.jvnet.jaxb.locator.util.LocatorUtils;
 
 
 /**
@@ -36,14 +47,8 @@ import jakarta.xml.bind.annotation.XmlType;
     "email",
     "address"
 })
-public class CustomerType {
-    /**
-     * Default constructor for CustomerType
-     */
-    public CustomerType() {
-        super();
-    }
-
+public class CustomerType implements Equals, HashCode, ToString
+{
 
     protected long id;
     @XmlElement(required = true)
@@ -51,6 +56,25 @@ public class CustomerType {
     @XmlElement(required = true)
     protected String email;
     protected AddressType address;
+
+    /**
+     * Default no-arg constructor
+     * 
+     */
+    public CustomerType() {
+        super();
+    }
+
+    /**
+     * Fully-initialising value constructor
+     * 
+     */
+    public CustomerType(final long id, final String name, final String email, final AddressType address) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.address = address;
+    }
 
     /**
      * Gets the value of the id property.
@@ -138,6 +162,133 @@ public class CustomerType {
      */
     public void setAddress(AddressType value) {
         this.address = value;
+    }
+
+    @Override
+    public String toString() {
+        final ToStringStrategy strategy = JAXBToStringStrategy.getInstance();
+        final StringBuilder buffer = new StringBuilder();
+        append(null, buffer, strategy);
+        return buffer.toString();
+    }
+
+    @Override
+    public StringBuilder append(ObjectLocator locator, StringBuilder buffer, ToStringStrategy strategy) {
+        strategy.appendStart(locator, this, buffer);
+        appendFields(locator, buffer, strategy);
+        strategy.appendEnd(locator, this, buffer);
+        return buffer;
+    }
+
+    @Override
+    public StringBuilder appendFields(ObjectLocator locator, StringBuilder buffer, ToStringStrategy strategy) {
+        {
+            long theId;
+            theId = this.getId();
+            strategy.appendField(locator, this, "id", buffer, theId, true);
+        }
+        {
+            String theName;
+            theName = this.getName();
+            strategy.appendField(locator, this, "name", buffer, theName, (this.name!= null));
+        }
+        {
+            String theEmail;
+            theEmail = this.getEmail();
+            strategy.appendField(locator, this, "email", buffer, theEmail, (this.email!= null));
+        }
+        {
+            AddressType theAddress;
+            theAddress = this.getAddress();
+            strategy.appendField(locator, this, "address", buffer, theAddress, (this.address!= null));
+        }
+        return buffer;
+    }
+
+    @Override
+    public boolean equals(ObjectLocator thisLocator, ObjectLocator thatLocator, Object object, EqualsStrategy strategy) {
+        if ((object == null)||(this.getClass()!= object.getClass())) {
+            return false;
+        }
+        if (this == object) {
+            return true;
+        }
+        final CustomerType that = ((CustomerType) object);
+        {
+            long lhsId;
+            lhsId = this.getId();
+            long rhsId;
+            rhsId = that.getId();
+            if (!strategy.equals(LocatorUtils.property(thisLocator, "id", lhsId), LocatorUtils.property(thatLocator, "id", rhsId), lhsId, rhsId, true, true)) {
+                return false;
+            }
+        }
+        {
+            String lhsName;
+            lhsName = this.getName();
+            String rhsName;
+            rhsName = that.getName();
+            if (!strategy.equals(LocatorUtils.property(thisLocator, "name", lhsName), LocatorUtils.property(thatLocator, "name", rhsName), lhsName, rhsName, (this.name!= null), (that.name!= null))) {
+                return false;
+            }
+        }
+        {
+            String lhsEmail;
+            lhsEmail = this.getEmail();
+            String rhsEmail;
+            rhsEmail = that.getEmail();
+            if (!strategy.equals(LocatorUtils.property(thisLocator, "email", lhsEmail), LocatorUtils.property(thatLocator, "email", rhsEmail), lhsEmail, rhsEmail, (this.email!= null), (that.email!= null))) {
+                return false;
+            }
+        }
+        {
+            AddressType lhsAddress;
+            lhsAddress = this.getAddress();
+            AddressType rhsAddress;
+            rhsAddress = that.getAddress();
+            if (!strategy.equals(LocatorUtils.property(thisLocator, "address", lhsAddress), LocatorUtils.property(thatLocator, "address", rhsAddress), lhsAddress, rhsAddress, (this.address!= null), (that.address!= null))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        final EqualsStrategy strategy = JAXBEqualsStrategy.getInstance();
+        return equals(null, null, object, strategy);
+    }
+
+    @Override
+    public int hashCode(ObjectLocator locator, HashCodeStrategy strategy) {
+        int currentHashCode = 1;
+        {
+            long theId;
+            theId = this.getId();
+            currentHashCode = strategy.hashCode(LocatorUtils.property(locator, "id", theId), currentHashCode, theId, true);
+        }
+        {
+            String theName;
+            theName = this.getName();
+            currentHashCode = strategy.hashCode(LocatorUtils.property(locator, "name", theName), currentHashCode, theName, (this.name!= null));
+        }
+        {
+            String theEmail;
+            theEmail = this.getEmail();
+            currentHashCode = strategy.hashCode(LocatorUtils.property(locator, "email", theEmail), currentHashCode, theEmail, (this.email!= null));
+        }
+        {
+            AddressType theAddress;
+            theAddress = this.getAddress();
+            currentHashCode = strategy.hashCode(LocatorUtils.property(locator, "address", theAddress), currentHashCode, theAddress, (this.address!= null));
+        }
+        return currentHashCode;
+    }
+
+    @Override
+    public int hashCode() {
+        final HashCodeStrategy strategy = JAXBHashCodeStrategy.getInstance();
+        return this.hashCode(null, strategy);
     }
 
 }
